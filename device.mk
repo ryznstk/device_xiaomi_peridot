5816cb08428e826295a6ce61add5d1561798189b
ryznstk <agusmhrd1k4@gmail.com>
2026-05-03 12:11:44 +0700

    HW COMP

--- device.mk
+++ device.mk
@@ -226,6 +226,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# HWUI
+TARGET_USES_VULKAN := true
+HWUI_COMPILE_FOR_PERF := true
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
