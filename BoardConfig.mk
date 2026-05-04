fc3a2607d7244cfe00b31b33b7ad8f17d6bdf0ec
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-05-04 18:28:49 +0700

    Update vendor security patch date
    Update vendor security patch date to December 1, 2025.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,6 +237,7 @@ include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include $(DEVICE_PATH)/sepolicy/SEPolicy-diag.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Vendor security patch
 VENDOR_SECURITY_PATCH := 2025-12-01
